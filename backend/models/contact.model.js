7363bde312fd41e0bd60304929e8478b79ea7561
tchamp295 <nyakundivictorn@gmail.com>
2025-01-21 17:24:52 +0300

    added backed dir

--- backend/models/contact.model.js
+++ backend/models/contact.model.js
@@ -1,31 +1,33 @@
 import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    interest: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-  },
-  interest: {
-    type: String,
-    required: true,
-  },
-  budget: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  {timestamps: true}
-});
+  { timestamps: true }
+);
 
 const Contact = mongoose.model("Contact", contactSchema);
 export default Contact;
